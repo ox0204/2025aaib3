81606bdf8d0138312fe552d4e4cf934dab1436d9
ox0204 <goodzzoey@gmail.com>
2025-11-12 11:53:34 +0800

    week01

---
+++ week01/week01_2b.py
@@ -0,0 +1,5 @@
+# week01_2b.py LeeCode 學習計畫 Python 版本2 find()函式
+# LeeCode 28. Find the Index of the First Occurrence in a String
+class Solution:
+    def strStr(self, haystack: str, needle: str) -> int:
+        return haystack.find(needle) # 只有一行
