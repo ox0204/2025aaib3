6295077a455e02125ea3a681c8c42b85a89ec674
ox0204 <goodzzoey@gmail.com>
2025-11-12 11:46:54 +0800

    week10

---
+++ week10/week10-2b.cpp
@@ -0,0 +1,10 @@
+/// week10-2b.cpp 厩策璸礶 Math 材1肈 (ノ计厩ㄓ秆)
+/// LeeCode 1523. Count Odd Numbers in an Interval Range
+class Solution {
+public:
+    int countOdds(int low, int high) {
+        int ans = (high-low) / 2; /// Τ, 琌计
+        if(low%2==1 || high%2==1) ans++; /// Τ1计 ++
+        return ans;
+    }
+};
